46865b4eec681be58ad2782278dd1378cef8f947
CoderBecca <rubeccayousaf.official@gmail.com>
2026-09-18 15:14:13 +0500

    Create tab_escape_sequence_table.cpp

---
+++ tab_escape_sequence_table.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Name\t\tMarks" << endl;
+    cout << "Rebecca\t\t95" << endl;
+    cout << "Ali\t\t88" << endl;
+    cout << "Sara\t\t90" << endl;
+    return 0;
+}
